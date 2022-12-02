174fd6906fa5ce31eb18d0d0ed62d7ca472a6a22
land <ljbelenky@gmail.com>
2022-12-02 13:16:49 -0700

    adding memoization and casting functions

--- minesweeper.py
+++ minesweeper.py
@@ -11,12 +11,15 @@ from sklearn.datasets import make_classification
 
 
 class Minesweeper:
-    def __init__(self, parameter_space):
+    def __init__(self, parameter_space, verbose=False):
         self.b = 3
         self.df = pd.DataFrame(columns=parameter_space.keys())
+        self.casting_functions = {k:v['func'] for k,v in parameter_space.items()}
         self.df['score'] = []
-        self.mms = MMS().fit(pd.DataFrame(parameter_space))
+        self.mms = MMS().fit(pd.DataFrame({k:v['range'] for k,v in parameter_space.items()}))
         self.dimensions = len(parameter_space)
+        self.previous_values = {}
+        self.verbose = verbose
         
     @property
     def bins(self):
@@ -28,7 +31,7 @@ class Minesweeper:
     
     @property
     def occupied_cells(self):
-        return list(set(self.df.apply(lambda x:np.digitize(x, self.bins)-1,
+        return list(set(self.df.iloc[:,:-1].apply(lambda x:np.digitize(x, self.bins)-1,
                                      result_type='reduce', axis=1).apply(tuple)))
     @property
     def exclude_cells(self):
@@ -45,29 +48,50 @@ class Minesweeper:
         return result
     
     def get_parameters(self):
-        scores = self.df.pop('score')
+        attempts = 0
         while True:        
             vacant_cells = list(self.all_cells-self.exclude_cells)
             if len(vacant_cells)==0:
                 self.b+=1
-                print(f'increasing b to {self.b}')
+                attempts +=1
+                if attempts>5:
+                    return None
+
+                if self.verbose: print(f'increasing b to {self.b}')
                 continue
                 
             chosen_cell = vacant_cells[np.random.randint(0,len(vacant_cells))]
-            chosen_values = tuple([(self.bins[i]+self.bins[i+1])/2 for i in chosen_cell])
-            self.df['score']=scores
-            return {k:v for k,v in zip(self.mms.feature_names_in_,
-                                       self.mms.inverse_transform([chosen_values])[0])}
-        
-    def update_values(self,values,score):
+            chosen_cell_center = tuple([(self.bins[i]+self.bins[i+1])/2 for i in chosen_cell])
+
+            values = {k:v for k,v in zip(self.mms.feature_names_in_, chosen_cell_center)}
+            parameters = {k:self.casting_functions[k](v) 
+                for k,v in zip(self.mms.feature_names_in_, self.mms.inverse_transform([chosen_cell_center])[0] )}
+
+            if str(parameters) in self.previous_values:
+                if self.verbose: print('Already done')
+                self.previous_values[str(parameters)]['values'] += [values]
+                self.update_values(parameters, score = self.previous_values[str(parameters)]['score'])
+
+                continue
+
+            else:
+                self.previous_values[str(parameters)] = {'values':[values]}
+                return parameters
+
+
+    def update_values(self, parameters, score):
+        self.previous_values[str(parameters)]['score'] = score
+        values = self.previous_values[str(parameters)]['values'][-1]
         values = pd.DataFrame(values, index=[0])
-        values = pd.DataFrame(data=self.mms.transform(values), columns=values.columns)
         values['score'] = score
-        
         self.df = pd.concat([self.df, values], ignore_index=True).reset_index(drop=True)
+        
     
     @property
     def history(self):
-        result = pd.DataFrame(data=self.mms.inverse_transform(self.df.iloc[:,:-1]), columns=self.mms.feature_names_in_)
-        result['score'] = self.df['score']
-        return result
+        if len(self.df)==0:
+            return pd.DataFrame(columns=self.mms.feature_names_in_+['score'])
+
+        return pd.DataFrame([eval(k)|{'score':v['score']} for k,v in self.previous_values.items()])
+
+
